db29ac08591b74926ed4a7ad905da29aa629ae97
Michael Antoni <michael@bluehive.com.ph>
2019-12-02 01:07:44 +0800

    Fix the pagination limit error make it 8 links only

--- src/components/supplier/Supplier.js
+++ src/components/supplier/Supplier.js
@@ -15,21 +15,31 @@ class Supplier extends Component {
         activePage: 1,
         itemsCountPerPage: 1,
         totalItemsCount: 10,
-        pageRangeDisplayed: 0
+        pageRangeDisplayed: 8
 	}
 
 	componentDidMount() {
 		this.getSuppliers();
+
+        // axios.get(`http://inventory.test/api/admin/supplier?page=${pageNumber}`)
+        //         .then(res => this.setState({
+        //                     suppliers: res.data.suppliers.data,
+        //                     activePage: res.data.suppliers.current_page,
+        //                     itemsCountPerPage: res.data.suppliers.per_page,
+        //                     totalItemsCount: res.data.suppliers.total,
+        //                 })
+        //             )
 	}
 		
 	// fetch all suppliers
     getSuppliers = async () => {
         this.setState({ loading: true });
         let res = await axios.get(`http://inventory.test/api/admin/supplier`);
-
-        console.log(res.data);
-        this.setState({ suppliers: res.data.suppliers.data, totalCount: res.data.count });
-        this.setState({ loading: false, pageRangeDisplayed: res.data.suppliers.total / res.data.suppliers.per_page});
+            this.setState({ 
+                suppliers: res.data.suppliers.data, 
+                totalCount: res.data.count,
+                loading: false 
+            });
     };
     
     // update archives: 1 if deleted
@@ -46,19 +56,18 @@ class Supplier extends Component {
         axios.get(`http://inventory.test/api/admin/supplier?page=${pageNumber}`)
                     .then(res => this.setState({
                                 suppliers: res.data.suppliers.data,
+                                activePage: res.data.suppliers.current_page,
                                 itemsCountPerPage: res.data.suppliers.per_page,
-                                totalItemsCount: Math.ceil(res.data.suppliers.total / res.data.suppliers.per_page),
-                                activePage: res.data.suppliers.current_page
+                                totalItemsCount: res.data.suppliers.total,
                             })
                         )
     }
 
-    // search supplier
+    // search  
     searchSupplier = (text) => {
         axios.get(``)
     } 
     
-
 	render() {
 		return (
 			<div>
@@ -81,7 +90,7 @@ class Supplier extends Component {
                     <div className="col-sm-12">
                         {this.state.loading ? (
                             <Spinner />
-                        ) : this.state.suppliers.length > 0 ? (
+                        ) : this.state.suppliers.length  > 0 ? (
                             <Fragment>
                                 <SupplierTable
                                     suppliers={this.state.suppliers}
@@ -89,21 +98,21 @@ class Supplier extends Component {
                                     moveToArchives={this.moveToArchives}
                                 />
 
-                            {this.state.totalCount > 10 && 
-
-                                <div className="d-flex justify-content-center">
-                                     <Pagination
-                                        className="pagination"
-                                        activePage={this.state.activePage}
-                                        itemsCountPerPage={this.state.itemsCountPerPage}
-                                        totalItemsCount={this.state.totalItemsCount}
-                                        pageRangeDisplayed={this.state.pageRangeDisplayed}
-                                        onChange={this.handlePageChange}
-                                        itemClass='page-item'
-                                        linkClass='page-link'
-                                    />
-                                </div>
-                            }
+                               {this.state.totalCount > 10 && (
+                                    <div className="d-flex justify-content-center">
+                                         <Pagination
+                                            className="pagination"
+                                            itemClass='page-item'
+                                            linkClass='page-link'
+                                            activePage={this.state.activePage}
+                                            itemsCountPerPage={this.state.itemsCountPerPage}
+                                            totalItemsCount={this.state.totalItemsCount}
+                                            pageRangeDisplayed={this.state.pageRangeDisplayed}
+                                            onChange={this.handlePageChange}
+                                        />
+                                    </div>
+                                )}
+                            
                             </Fragment>
                         ) : (
                             <h1 align="center" className="mt-5">There's No Data to show...</h1>
